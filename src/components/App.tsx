7b49f1b61a77630711586d543a48555060831244
Marcel Puetz <marcel.putz@konux.de>
2019-01-16 14:35:10 +0100

    Add global styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import styled from 'styled-components/macro';
+
+import { GlobalStyles } from '../styles/globalStyles';
 
 export class App extends Component {
   render() {
-    return <div className="App">App</div>;
+    return (
+      <StyledApp>
+        App
+        <GlobalStyles />
+      </StyledApp>
+    );
   }
 }
+
+const StyledApp = styled.div``;
